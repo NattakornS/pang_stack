bf100bfbc7d338a1add0977866976c5eaccfc248
Nattakorn Sanpabopit <nattakorn.kingman@gmail.com>
2020-12-23 14:15:33 +0700

    add nginx for reverse proxy

--- cms/api/user-info/controllers/user-info.js
+++ cms/api/user-info/controllers/user-info.js
@@ -8,11 +8,11 @@ const tf = require("@tensorflow/tfjs-node");
 module.exports = {
     
     async predict(ctx) {
-        const model = await tf.node.loadSavedModel("/srv/model/");
-        let result = model.predict([2,4,1,1,175000.0,97,47,10000.0])
-        if (result) {
-            return result
-        }
+        // const model = await tf.node.loadSavedModel("/srv/model/");
+        // let result = model.predict([2,4,1,1,175000.0,97,47,10000.0])
+        // if (result) {
+        //     return result
+        // }
         return 'Predict!';
     },
 };
